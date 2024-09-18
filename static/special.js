6f0419115eaf8c28a1e2042c4ac19bc88372c300
peaceinharsh <167442929+peaceinharsh@users.noreply.github.com>
2024-09-18 20:11:38 +0530

    Add files via upload

---
+++ static/special.js
@@ -0,0 +1,40 @@
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+});
+
+document.addEventListener('click', e => {
+    cursor.classList.add("expand");
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500);
+});
+
+    window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+// ...nav
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "500px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+document.addEventListener("click", function(event) {
+  if (!event.target.closest(".sidenav") && !event.target.closest(".header")) {
+    closeNav();
+  }
+});
